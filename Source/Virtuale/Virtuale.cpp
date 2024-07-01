d18b0f6f5798d46d628d9855c747cc9f1ebb9e0a
ZachLovato <zlovato@student.neumont.edu>
2024-07-01 14:03:27 -0600

    merge

---
+++ Source/Virtuale/Virtuale.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Virtuale.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Virtuale, "Virtuale" );
