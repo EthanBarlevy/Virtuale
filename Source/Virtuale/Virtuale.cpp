60fb4ab055a27a66ec3700ad0c72f5da8a7034e4
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2024-07-02 14:42:15 -0600

    Added the plugin content
    we totally arent stealing lol

--- Source/Virtuale/Virtuale.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Virtuale.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Virtuale, "Virtuale" );
